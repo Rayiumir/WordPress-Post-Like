e8037d0e6312801106bdbed831afcb474699eacb
Raymond Baghumian <swordart2050@gmail.com>
2024-02-21 17:17:57 +0330

    Bug Fix and Create Function Install Table likes

--- inc/functions.php
+++ inc/functions.php
@@ -1,1 +1,28 @@
 <?php
+
+defined( 'ABSPATH' ) || exit;
+
+function rayium_post_like_install() {
+    
+    global $wpdb;
+    
+    $table_wp_likes = $wpdb->prefix . 'likes';
+    
+    $sql = "
+    CREATE TABLE `{$table_wp_likes}` (
+        `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+        `post_id` bigint(20) unsigned NOT NULL,
+        `user_id` bigint(20) unsigned NOT NULL,
+        `ip` varchar(15) NOT NULL,
+        `liked` tinyint(1) NOT NULL DEFAULT 1,
+        `created_at` datetime NOT NULL,
+        PRIMARY KEY (`ID`),
+        KEY `post_id` (`post_id`),
+        KEY `user_id` (`user_id`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+      ";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+    dbDelta($sql);
+}
--- wp_likes.php
+++ wp_likes.php
@@ -1,24 +1,44 @@
-<?php   
+<?php 
+
 /*
  * Plugin Name:       Rayium Wordpress Post Like 
  * Plugin URI:        https://github.com/Rayiumir/WordPress-Post-Like
  * Description:       A Plugin Post Like for Wordpress
  * Version:           1.0.0
  * Requires at least: 6.0.0
- * Requires PHP:      7.2
+ * Requires PHP:      7.1
  * Author:            Raymond Baghumian
  * Author URI:        https://github.com/Rayiumir/WordPress-Post-Like
+ * Text Domain: rayium-post-like
+ * Domain Path: /languages
  */
 
  defined('ABSPATH') || exit;
 
-define("Rayium_URL", plugin_dir_url( __FILE__ ));
-define("Rayium_PATH", plugin_dir_path( __FILE__ ));
-
-define('RAYIUM_WP_LIKES_INC', Rayium_PATH . 'inc/');
-define('RAYIUM_WP_LIKES_CSS', Rayium_URL . 'css/');
-define('RAYIUM_WP_LIKES_JS', Rayium_URL . 'js/');
-
-// Calling Files
-
-require(RAYIUM_WP_LIKES_INC . 'function.php');
+ define("RAYIUM_URL", plugin_dir_url( __FILE__ ));
+ define("RAYIUM_PATH", plugin_dir_path( __FILE__ ));
+ 
+ define('RAYIUM_WP_LIKES_INC', RAYIUM_PATH . 'inc/');
+ define('RAYIUM_WP_LIKES_CSS', RAYIUM_URL . 'css/');
+ define('RAYIUM_WP_LIKES_JS', RAYIUM_URL . 'js/');
+ 
+ define( 'RAYIUM_WP_LIKES_VERSION', '1.0.0' );
+ 
+ // Calling Files
+ 
+ require_once(RAYIUM_WP_LIKES_INC . 'functions.php');
+ 
+ // Database Post Likes
+ 
+ global $wpdb;
+ $wpdb->wp_likes = $wpdb->prefix  . 'wp_likes';
+ 
+ // Languages
+ 
+ add_action( 'plugins_loaded', function(){
+     load_plugin_textdomain( 'rayium-post-like', false, dirname( plugin_basename( __FILE__ ) ) . '/languages');
+ });
+ 
+ // Activation Install
+ 
+ register_activation_hook( __FILE__, 'rayium_post_like_install' );
