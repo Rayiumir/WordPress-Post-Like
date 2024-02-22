ddc3a5deb3886ad3f63fe2e3d9a1c24e24c035ed
Raymond Baghumian <swordart2050@gmail.com>
2024-02-23 02:28:09 +0330

    Complete jQuery Aaja Button Like

--- js/likes.js
+++ js/likes.js
@@ -2,6 +2,14 @@ jQuery(document).ready(function($){
 
     $('.LikePost').click(function(){
 
+        if( $(this).hasClass('loading') ){
+            return;
+        }
+
+        let count   = $(this).find('.LikePost');
+        let btn      = $(this);
+        let msg     = $(this).next();
+
         $.ajax({
             url : rayium.ajax_url,
             type : 'POST',
@@ -12,20 +20,56 @@ jQuery(document).ready(function($){
                 like : ! $(this).hasClass('LikePosted')
             },
 
+            beforeSend: function(){
+                $(btn).addClass('loading');
+                $(msg).removeClass('show error').text('');
+            },
+            complete: function(){
+                $(btn).removeClass('loading');
+            },
             success: function( result ){
                 if( result.success ){
-                    console.log('success');
+                    if( result.data.liked ){
+                        $(btn).addClass('LikePost');
+                    }else{
+                        $(btn).removeClass('LikePosted');
+                    }
+                    $(count).text( `(${result.data.count})` );
                 }else{
-                    console.log('not success');
+                    
                 }
+
+                $(msg)
+                .text( result.data.message )
+                .addClass(result.success ? 'success' : 'error')
+                .addClass('show')
+                .delay(3000)
+                .queue( function( next ){
+                    $(this).removeClass('show error success')
+                    next();
+                } );
 
             },
             error: function( xhr, status, http_error_description ){
-                console.log(xhr.responseJSON);
-                console.log(status);
-                console.log(http_error_description);
-            },
 
+                let message = xhr.responseJSON !== undefined ? xhr.responseJSON.data.message : false;
+
+                if( ! message && http_error_description !== undefined ){
+                    message = http_error_description;
+                }
+
+                if( message !== undefined ){
+                    $(msg)
+                    .text( message )
+                    .addClass( 'error')
+                    .addClass('show')
+                    .delay(3000)
+                    .queue( function( next ){
+                        $(this).removeClass('show error success')
+                        next();
+                    } );
+                }
+            },
         });
 
     });
