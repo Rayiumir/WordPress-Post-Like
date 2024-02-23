82b9ffae4890ef6b07702112bd7648c1045cbd06
Raymond Baghumian <swordart2050@gmail.com>
2024-02-24 02:21:02 +0330

    Adding Tag p for Compatibility with Themes

--- inc/functions.php
+++ inc/functions.php
@@ -68,12 +68,12 @@ function rayium_button_post_like($content){
     $liked_class    = rayium_is_like_post( $post_id, get_current_user_id() ) ? 'post-liked' : '';
     $nonce = wp_create_nonce( 'post-like' . $post_id );
     
-    $button = "<button type='button' class='post-like $liked_class' data-id='$post_id' data-nonce='$nonce'>
-        <svg width='16' height='16' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><style>.spinner_ajPY{transform-origin:center;animation:spinner_AtaB .75s infinite linear}@keyframes spinner_AtaB{100%{transform:rotate(360deg)}}</style><path d='M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z' opacity='.25'/><path d='M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z' class='spinner_ajPY'/></svg> 
-     $text 
-     <span class='like-count'>$like_count</span>
-    </button>
-    <span class='like-message'></span>
+    $button = "<p><button type='button' class='post-like $liked_class' data-id='$post_id' data-nonce='$nonce'>
+    <svg width='16' height='16' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><style>.spinner_ajPY{transform-origin:center;animation:spinner_AtaB .75s infinite linear}@keyframes spinner_AtaB{100%{transform:rotate(360deg)}}</style><path d='M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z' opacity='.25'/><path d='M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z' class='spinner_ajPY'/></svg> 
+ $text 
+ <span class='like-count'>$like_count</span>
+</button>
+<span class='like-message'></span></p>
     ";
 
     return $content . $button;
